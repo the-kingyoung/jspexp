5ec74243bd187d0dbf525b3d5d512a57b08538f7
the-kingyoung <cris1945215@gmail.com>
2021-08-06 21:57:00 +0900

    baekjoon 0806

---
+++ src/baekjoon/baekjoon0013.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class baekjoon0013 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int code = scan.next().charAt(0);
+		
+		System.out.println("아스키 코드는 " + code);
+	}
+}
