66d8a0f37792932b52b8ae012d2e90605e158516
the-kingyoung <cris1945215@gmail.com>
2021-08-16 21:02:21 +0900

    add

---
+++ src/baekjoon/baekjoon0020.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class baekjoon0020 {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int n = scan.nextInt();
+
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			while (n % i == 0) {
+				System.out.println(i);
+				n /= i;
+			}
+		}
+		if (n != 1) {
+			System.out.println(n);
+		}
+	}
+
+}
