f38888653e09c9f1b6ed1c60eba205ce54421a45
the-kingyoung <cris1945215@gmail.com>
2021-10-11 19:50:42 +0900

    add

--- src/baekjoon/NEWLEC3.sql
+++ src/baekjoon/NEWLEC3.sql
@@ -10,8 +10,17 @@ SELECT * FROM NOTICE_NEWLEC WHERE REGEXP_LIKE(TITLE,'01[016-9]-\d{3,4}-\d{4}');
 --019-232-2345
 ^01[016-9]-\d{3,4}-\d{4}$
 
+--[0-9a-zA-Z] = \w
+--* => 0개 이상
+--+ => 1개 이상
+--\D\w*@\Dw*.[org|net|com]
+SELECT * FROM NOTICE_NEWLEC;
+SELECT * FROM MEMBER_NEWLEC mn ;
+
+
 
 
 
 INSERT INTO NOTICE_NEWLEC(ID, TITLE, WRITER_ID, CONTENT, REGDATE, HIT, FILES)
 VALUES('7','선반-접시-그릇 이 세가지요~~', 'okay', '연락처를 남깁니다.', sysdate, 23,'');
+
