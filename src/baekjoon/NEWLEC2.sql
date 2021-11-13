b00c6cfc44d42a25be19422cfc45499df735a31a
the-kingyoung <cris1945215@gmail.com>
2021-11-13 18:20:06 +0900

    Add

--- src/baekjoon/NEWLEC2.sql
+++ src/baekjoon/NEWLEC2.sql
@@ -16,6 +16,7 @@ SELECT * FROM MEMBER_NEWLEC;
 SELECT ID "USER ID", NAME, PWD FROM MEMBER_NEWLEC mn ;
 
 UPDATE MEMBER_NEWLEC SET PWD='222', NAME='손오공' WHERE ID='dragon' ;
+UPDATE MEMBER_NEWLEC SET BOSS_ID='yong' WHERE NAME='강호동' ;
 DELETE MEMBER_NEWLEC WHERE ID='test';
 
 -- 트랜잭션이란? 업무 실행단위 / 논리 명령단위 / 개념상의	--> commit , rollback 
